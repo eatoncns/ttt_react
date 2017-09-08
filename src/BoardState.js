a27bc2bedb24f44f0f82eeeba1b2ed17126456fc
Philip Meier <philip.meier06@googlemail.com>
2017-09-08 10:09:23 +0100

    Simplify board state
    Trying a more functional approach here with minimal state from
    which functions can derive other info

--- src/BoardState.js
+++ src/BoardState.js
@@ -1,8 +1,16 @@
-export const init = (dimension) => ({ currentPlayer: 'X', marks: Array(dimension*dimension).fill('') });
+export const init = (dimension) => ({ marks: Array(dimension*dimension).fill('') });
 
 export const update = (boardState, index) => {
   const newBoardMarks = boardState.marks.slice();
-  newBoardMarks[index] = boardState.currentPlayer;
-  const newCurrentPlayer = boardState.currentPlayer === 'X' ? 'O' : 'X'; 
-  return { currentPlayer: newCurrentPlayer, marks: newBoardMarks };
+  newBoardMarks[index] = currentPlayer(boardState);
+  return { marks: newBoardMarks };
+};
+
+const currentPlayer = (boardState) => {
+  const populatedSpaces = boardState.marks.filter((mark) => mark !== '');
+  return isEven(populatedSpaces.length) ? 'X' : 'O';
+};
+
+const isEven = (number) => {
+  return (number % 2) === 0;
 };
