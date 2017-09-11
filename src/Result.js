c4e948d44ab01856d71d29376411d818162250c4
Philip Meier <philip.meier06@googlemail.com>
2017-09-11 11:11:35 +0100

    Render a Result in BoardContainer

---
+++ src/Result.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+function Result(props) {
+  return null;
+}
+
+Result.propTypes = {
+  marks: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Result;
