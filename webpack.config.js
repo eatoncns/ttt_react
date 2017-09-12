f04cb26705dda56ae66f0c73533b9c7fa736ae40
Philip Meier <philip.meier06@googlemail.com>
2017-09-12 15:10:29 +0100

    Working build and start

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
+  }
+};
+
+module.exports = config;
