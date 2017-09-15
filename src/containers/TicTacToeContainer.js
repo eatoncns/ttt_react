78d4c2893d205a425a646b8d629a000adea2c0cf
Philip Meier <philip.meier06@googlemail.com>
2017-09-15 11:56:38 +0100

    Add previous games info to TicTacToe container state

--- src/containers/TicTacToeContainer.js
+++ src/containers/TicTacToeContainer.js
@@ -3,20 +3,25 @@ import History from 'components/History';
 import BoardContainer from 'containers/BoardContainer'
 
 class TicTacToeContainer extends Component {
-  render() {
+  constructor(props) {
+    super(props);
     const games = [{timestamp: "14/09/2017, 09:45:48",
                     result: "X win",
                     finalMarks: ['X','X','X','O','O','','','','']}, 
                    {timestamp: "14/09/2017, 10:00:54",
                     result: "O win",
                     finalMarks: ['O','O','O','X','X','','X','','']}]; 
+    this.state = { games: games };
+  }
+
+  render() {
     return (
       <div>
         <div className="container">
           <h1>Tic-Tac-Toe</h1>
           <BoardContainer dimension={3} />
         </div>
-        <History games={games} />
+        <History games={this.state.games} />
       </div>
     );
   }
