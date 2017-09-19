ae285b111f2cd06d4612be4cd789637108abaf89
Philip Meier <philip.meier06@googlemail.com>
2017-09-19 13:06:59 +0100

    Update TicTacToeContainer to POST game result to backend

--- src/containers/TicTacToeContainer.js
+++ src/containers/TicTacToeContainer.js
@@ -7,13 +7,13 @@ class TicTacToeContainer extends Component {
   constructor(props) {
     super(props);
     this.state = HistoryState.init();
+    this.endpoint = 'http://ttt-history-dev.eu-west-2.elasticbeanstalk.com/history';
   }
 
   componentDidMount() {
-    const endpoint = 'http://ttt-history-dev.eu-west-2.elasticbeanstalk.com/history';
-    fetch(endpoint).then((response) => this.checkResponse(response))
-                   .then((responseJson) => this.populateInitialState(responseJson))
-                   .catch((error) => console.log(error.message));
+    fetch(this.endpoint).then((response) => this.checkResponse(response))
+                        .then((responseJson) => this.populateInitialState(responseJson))
+                        .catch((error) => console.log(error.message));
   }
 
   checkResponse(response) {
@@ -32,7 +32,22 @@ class TicTacToeContainer extends Component {
   }
 
   onGameOver(finalMarks) {
-    this.setState(HistoryState.update(this.state, finalMarks, this.dateTimeNow()));
+    const timestamp = this.dateTimeNow();
+    this.setState(HistoryState.update(this.state, finalMarks, timestamp));
+    this.pushGameToBackend(finalMarks, timestamp);
+  }
+
+  pushGameToBackend(finalMarks, timestamp) {
+    fetch(this.endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        timestamp: timestamp,
+        board: finalMarks
+      })
+    });
   }
 
   dateTimeNow() {
